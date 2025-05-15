324fcda53f8b3c440f519b8e89cfbd1faf894056
Makar <112917238+MFrolov3@users.noreply.github.com>
2025-05-16 02:26:17 +0700

    Update segment3D.h

--- Intersection_segments/segment3D.h
+++ Intersection_segments/segment3D.h
@@ -17,7 +17,7 @@ public:
     Segment3D(const Vector3D& start_arg, const Vector3D& end_arg); //constructor of the segment class
     Vector3D get_start() const; //getters of segment class fields
     Vector3D get_end() const;
-    Vector3D Intersect(const Segment3D& segment) const; //method of the segment class for separating the intersection point of two segments
+    Vector3D Intersect(const Segment3D& segment) const; 
 };
 
 
